d7533bde9e600c5fc1c930678d332efa54466ab6
Josh Newsom <newsommeister@gmail.com>
2016-07-07 12:38:57 -0600

    Starting Reddit Clone refactor

---
+++ Unit_2/01_a_new_structure/main.js
@@ -0,0 +1,72 @@
+var app = angular.module('RedditClone', ['ngAnimate']);
+
+app.controller('RedditController', function($scope) {
+    $scope.posts = [];
+    $scope.sort = {};
+    $scope.sort.sortBy = 'upVotes';
+
+    $scope.Date = function(arg) {
+        return moment(arg);
+    };
+
+    $scope.momentize = function(date) {
+        return moment(date).calendar();
+    };
+
+    $scope.showNewPostForm = function() {
+        $scope.post.newPost = !$scope.post.newPost;
+    };
+
+    $scope.submitPost = function(event) {
+        event.preventDefault();
+        $scope.post.date = moment();
+        $scope.posts.push(angular.copy($scope.post));
+        $scope.resetPost();
+        $scope.newPostForm.$setUntouched();
+    };
+
+    $scope.resetPost = function() {
+        $scope.post = {
+            title: null,
+            author: null,
+            imageURL: null,
+            description: null,
+            date: null,
+            comments: [],
+            newPost: false,
+            upVotes: 0,
+            showComments: false,
+            newComment: false
+        };
+    };
+
+    $scope.upVote = function(post) {
+        post.upVotes += 1;
+    };
+
+    $scope.downVote = function(post) {
+        post.upVotes -= 1;
+    };
+
+    $scope.showComments = function(post) {
+        post.showComments = !post.showComments;
+    };
+
+    $scope.newComment = function(post) {
+        post.newComment = !post.newComment;
+    };
+
+    $scope.submitComment = function(event, post, form) {
+        event.preventDefault();
+        post.comments.push({
+            author: post.commentAuthor,
+            text: post.commentText
+        });
+        post.commentAuthor = '';
+        post.commentText = '';
+        post.newComment = false;
+        form.$setUntouched();
+    };
+
+    $scope.resetPost();
+});
