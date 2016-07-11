98578666c23528c078bdb53fb231b7b4cfcb3206
Josh Newsom <newsommeister@gmail.com>
2016-07-11 11:03:58 -0600

    Getting API data back

--- Unit_2/07_movie_search_app/js/controllers.js
+++ Unit_2/07_movie_search_app/js/controllers.js
@@ -1,3 +1,22 @@
-app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
-    
+app.controller('SearchController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+    $rootScope.search = {
+        results: []
+    }
+    $scope.search = {
+        title: '',
+        search: function(title) {
+            console.log('searching...');
+            $http.get('http://www.omdbapi.com/?s=' + title + '&type=movie')
+                .then(function(data) {
+                    $rootScope.search = {
+                        results: data.data.Search
+                    };
+                });
+        }
+    };
+}]);
+
+app.controller('HomeController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    console.log('using HomeController');
+    console.log($rootScope);
 }]);
