803ce3d6ccd59895ee8d4666d51d950558be40c2
Josh Newsom <newsommeister@gmail.com>
2016-07-14 16:09:25 -0600

    Shopping Cart - Main template

--- Unit_2/12_shopping_cart_app/js/app.js
+++ Unit_2/12_shopping_cart_app/js/app.js
@@ -1,1 +1,18 @@
 var app = angular.module('ShoppingCartApp', ['ngRoute', 'ngAnimate']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/teaShop.html',
+            controller: 'teaShopController'
+        })
+        .when('/checkout', {
+            templateUrl: 'partials/checkout.html',
+            controller: 'checkoutController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+app.$inject = ['$routeProvider'];
