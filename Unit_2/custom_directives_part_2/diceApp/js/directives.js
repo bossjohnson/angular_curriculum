8ab32181daaca977d1a75c86fe11bff38ea94832
Josh Newsom <newsommeister@gmail.com>
2016-07-14 14:45:18 -0600

    Dice app coming along nicely

---
+++ Unit_2/custom_directives_part_2/diceApp/js/directives.js
@@ -0,0 +1,54 @@
+app.directive('jnDice', function() {
+    return {
+        templateUrl: 'partials/die.html',
+        restrict: 'E',
+        scope: {},
+        link: function(scope, element, attrs) {
+            // scope.value = roll();
+            switch (roll()) {
+                case 1:
+                    scope.face = 'dieFaces/onePip.png';
+                    break;
+                case 2:
+                    scope.face = 'dieFaces/twoPips.png';
+                    break;
+                case 3:
+                    scope.face = 'dieFaces/threePips.png';
+                    break;
+                case 4:
+                    scope.face = 'dieFaces/fourPips.png';
+                    break;
+                case 5:
+                    scope.face = 'dieFaces/fivePips.png';
+                    break;
+                case 6:
+                    scope.face = 'dieFaces/sixPips.png';
+                    break;
+                default:
+                    console.log("FUCK");
+            }
+        }
+    };
+
+    function roll() {
+        console.log('rolling...');
+        return Math.ceil(Math.random() * 6);
+    }
+});
+
+app.directive('jnRoll', function() {
+    return {
+        restrict: 'A',
+        controller: roller
+    };
+
+    function roller($scope, $rootScope) {
+        $scope.roll = function(numDice) {
+            $rootScope.view.dice = [];
+            for (var i = 0; i < numDice; i++) {
+                $rootScope.view.dice.push(null);
+            }
+        };
+    }
+    roller.$inject = ['$scope', '$rootScope'];
+});
